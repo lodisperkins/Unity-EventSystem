5869a05018a1d8cb4dd010a44a44afb459f7a4c1
Lodis Perkins <lodisperkins44@gmail.com>
2019-09-17 15:30:25 -0500

    Initial commit

---
+++ Assets/Lodis/CubeBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour {
+    [SerializeField]
+    Lodis.Event OnClick;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void changeColor()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+        {
+            OnClick.Raise();
+        }
+	}
+}
